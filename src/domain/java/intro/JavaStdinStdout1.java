0d6bc003af8f2b06cf09625c44eaa272633476e8
kumar.nipun <kumar.nipun@tftus.com>
2016-01-28 20:18:14 +0530

    Added new file

---
+++ src/domain/java/intro/JavaStdinStdout1.java
@@ -0,0 +1,33 @@
+/*
+ Problem Statement:
+ In most of the HackerRank challenges, you need to read input from stdin (standard input) and write your output in stdout (standard output).
+One way to take input from stdin is to use the Scanner class and read in from System.in.
+You can write your output to stdout by simply using the System.out.println(String) function.
+In this problem, you need to read 3 integers from stdin and print them in stdout.
+
+Sample input:
+42
+100
+125
+Sample output:
+42
+100
+125
+To make the problem easier for you, part of the code is already provided in the editor.
+ */
+package domain.java.intro;
+
+import java.util.Scanner;
+
+public class JavaStdinStdout1 {
+	public static void main(String[] args) {
+	      Scanner sc=new Scanner(System.in);
+	      int a=sc.nextInt();
+	      int b=sc.nextInt();
+	      int c=sc.nextInt();	         
+	      System.out.println(a);
+	      System.out.println(b);
+	      System.out.println(c);
+	    }
+
+}
---
+++ src/domain/java/intro/WelcomeJava.java
@@ -0,0 +1,24 @@
+/*
+ Problem Statement:
+ Welcome to the world of Java! Just print "Hello World." and "Hello Java." in two separate lines to complete this challenge.
+The code stub in the editor already creates the main function and solution class. 
+All you have to do is copy and paste the following lines inside the main function.
+
+System.out.println("Hello World.");
+System.out.println("Hello Java.");
+
+Sample Output:
+Hello World.
+Hello Java.
+
+ */
+package domain.java.intro;
+
+public class WelcomeJava {
+	
+	public static void main(String args[]){
+        System.out.println("Hello World.");
+        System.out.println("Hello Java.");
+    }
+
+}
